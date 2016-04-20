01d9a2a56dcb838c663cc2c9e2926d3e892fe7e8
Gege Pincin <gege@pincin.com>
2016-04-20 14:30:48 -0400

    starter tests, 100% coverage

--- test/dequeue.test.js
+++ test/dequeue.test.js
@@ -1,9 +1,45 @@
-'use strict'
-
+var Code = require('code')
 var Lab = require('lab')
+var Seneca = require('seneca')
+var Dequeue = require('../lib/dequeue')
 
 var lab = exports.lab = Lab.script()
+var expect = Code.expect
+var before = lab.before
+var suite = lab.suite
+var it = lab.it
 var describe = lab.describe
 
-describe('No Tests', () => {
+var Service = Seneca()
+
+Service.use(Dequeue)
+
+suite('nodezoo-dequeue suite tests ', function () {
+  before({}, function (done) {
+    Service.ready(function (err) {
+      if (err) {
+        return process.exit(!console.error(err))
+      }
+      Service.add('role:info,req:part', function (args, done) {
+        done()
+      })
+      done()
+    })
+  })
+})
+
+describe('nodezoo-dequeue tests', () => {
+  it('Plugin Recieves Messages', function (done) {
+    Service.act({role: 'updater', info: 'update', name: 'honeybadger'}, function (err, respond) {
+      expect(err).to.not.exist()
+      expect(respond).to.exist()
+      done(err)
+    })
+  })
+  it('Plugin Responds to Messages', function (done) {
+    Service.act({role: 'updater', info: 'update', name: 'honeybadger'}, function (err, respond) {
+      expect(respond).to.exist()
+      done(err)
+    })
+  })
 })
