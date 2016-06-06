1eadcd4ce86e92f32e3fc7438dff4eb63f95820d
malex <mircea.alexandru@gmail.com>
2016-06-06 10:54:42 +0300

    added env variables

--- srv/start.js
+++ srv/start.js
@@ -11,12 +11,14 @@ var opts = {
     'redis-queue': {
       timeout: 22222,
       type: 'redis-queue',
-      host: 'localhost',
-      port: 6379
+      host: envs.DEQUEUE_REDIS_HOST || 'localhost',
+      port: envs.DEQUEUE_REDIS_PORT || '6379'
     }
   },
   mesh: {
-    auto: true
+    auto: true,
+    host: envs.DEQUEUE_HOST || '127.0.0.1',
+    bases: [envs.BASE_HOST || '127.0.0.1:39999']
   }
 }
 
